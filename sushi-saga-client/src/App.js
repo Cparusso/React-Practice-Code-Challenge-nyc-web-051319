1a200ba8c62aa9056130a491b1eeb691dd1a0fd9
Charlie Russo <cparusso@gmail.com>
2019-07-23 16:05:56 -0400

    post-lecture

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,54 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  state={
+    sushis: [],
+    startIndex: 0,
+    eatenSushi: [],
+    wallet: 100
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(resp => resp.json())
+      .then(sushiObjs => this.setState({sushis: sushiObjs}))
+  }
+
+  moreSushi = () => {
+    this.setState({
+      startIndex: this.state.startIndex + 4
+    })
+  }
+
+  addPlate = (sushiObj) => {
+    this.setState({
+      eatenSushi: [...this.state.eatenSushi, sushiObj]
+    })
+  }
+
+  buyWonSoosh = (cost) => {
+    this.setState({
+      wallet: this.state.wallet - cost
+    })
+  }
 
   render() {
+    const { sushis, startIndex, eatenSushi, wallet } = this.state
+
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer
+          sushis={ sushis }
+          startIndex={ startIndex }
+          moreSushi={ this.moreSushi }
+          addPlate={ this.addPlate }
+          buyWonSoosh={ this.buyWonSoosh }
+          wallet={ wallet }
+        />
+        <Table
+          eatenSushi={ eatenSushi }
+          wallet={ wallet }
+        />
       </div>
     );
   }
